e6ca48bbeeeb8d547898728ebab2887ea020afb5
Hritik Pundir <162597490+hritikpundir@users.noreply.github.com>
2026-03-22 22:33:13 +0530

    Time: 20 ms (84.77%) | Memory: 61.6 MB (34.39%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        int left = 0, right = nums.size() - 1, target = nums.size() - k;
-        while (left <= right) {
-            int pivot = partition(nums, left, right);
-            if (pivot == target) return nums[pivot];
-            else if (pivot < target) left = pivot + 1;
-            else right = pivot - 1;
+        int n = nums.size();
+        priority_queue<int,vector<int>, greater<int>> pq; // min-heap
+        for(int i=0; i<k; ++i){
+           pq.push(nums[i]);
         }
-        return -1;
-    }
-    int partition(vector<int>& nums, int left, int right) {
-        int pivot = nums[right], i = left;
-        for (int j = left; j < right; ++j) {
-            if (nums[j] < pivot) swap(nums[i++], nums[j]);
+        for(int i=k; i<n; i++){
+            if(nums[i] > pq.top()){
+                pq.pop();
+                pq.push(nums[i]);
+            }
         }
-        swap(nums[i], nums[right]);
-        return i;
+        return pq.top();
     }
 };
