e8c8af928121ddfd4ec835548c5fad6c8656d59f
Hritik Pundir <162597490+hritikpundir@users.noreply.github.com>
2026-09-17 13:00:01 +0530

    Time: 39 ms (94.58%) | Memory: 29.1 MB (46.01%) - LeetSync

---
+++ 15-3sum/3sum.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> res;
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < n - 2; i++) {
+            // to remove duplicacy
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            int left = i + 1;
+            int right = n - 1;
+            int sum = -nums[i];
+            while (left < right) {
+                int s = nums[left] + nums[right];
+                if (s == sum) {
+                    res.push_back({nums[i], nums[left], nums[right]});
+                    left++;
+                    right--;
+
+                    // to avoid duplicacy
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+                }
+
+                else if (s < sum) {
+                    left++;
+                } else
+                    right--;
+            }
+        }
+        return res;
+    }
+};
